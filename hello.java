95d81d84c4a8e117d6b962b0f405b44a2a56d6f9
root <inbasharvan@gmail.com>
2021-06-06 06:45:06 +0000

    Master changes in println

--- hello.java
+++ hello.java
@@ -7,7 +7,7 @@ public static void main(String args[])
 {
 
 <<<<<<< HEAD
-System.out.println("hello master editied");
+System.out.println("hello master_1 editied");
 =======
 System.out.println("hello from child branch");
 >>>>>>> child
