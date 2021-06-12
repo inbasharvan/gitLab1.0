6c35440fb884c893e627c44a3253cbb581d1bff2
root <inbasharvan@gmail.com>
2021-06-12 03:10:02 +0000

    Resolved Confilts

--- hello.java
+++ hello.java
@@ -5,13 +5,8 @@ class Hello
 public static void main(String args[])
 
 {
-
-<<<<<<< HEAD
 System.out.println("hello master_1 editied");
-=======
 System.out.println("hello from child branch");
->>>>>>> child
-
 }
 
 }
